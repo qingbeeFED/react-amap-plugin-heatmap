3755b1be9f7b4d306fc67c3fda865a2a7777bd76
singcl <24661881@qq.com>
2018-09-03 12:01:51 +0800

    对象解构支持

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ var webpackConfig = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: ['transform-object-rest-spread']
         }
       }
     ]
